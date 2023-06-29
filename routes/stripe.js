817477b4c05244a6263aa794e03cf14f824fddbd
Carnage <akotoxmpimbo@gmail.com>
2023-06-30 03:57:06 +0800

    dsffsdf

--- routes/stripe.js
+++ routes/stripe.js
@@ -64,8 +64,8 @@ router.post("/create-checkout-session", async (req, res) => {
     line_items,
     mode: "payment",
     customer: customer.id,
-    success_url: "https://4e2e-38-98-135-218.ngrok-free.app/stripe/checkout-success",
-    cancel_url:  "https://4e2e-38-98-135-218.ngrok-free.app/stripe/cancel",
+    success_url: "https://paymentorders-production.up.railway.app/stripe/checkout-success",
+    cancel_url:  "https://paymentorders-production.up.railway.app/stripe/cancel",
   });
 
   // res.redirect(303, session.url);
